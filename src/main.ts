51c0ec3d1501e69e57f116664573524693d6a500
Wojciech Marek <marek.wojciech@outlook.com>
2023-12-15 12:41:22 +0100

    feat: add vercel analytics

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
-import './app.css'
-import App from './App.svelte'
+import "./app.css";
+import App from "./App.svelte";
+import { inject } from "@vercel/analytics";
+
+inject();
 
 const app = new App({
-  target: document.getElementById('app'),
-})
+  target: document.getElementById("app"),
+});
 
-export default app
+export default app;
