e50369de1913ad79830b9fce90b6e4c0d73554a1
Wojciech Marek <marek.wojciech@outlook.com>
2024-06-23 13:22:56 +0200

    chore: update api

--- api/hello.ts
+++ api/hello.ts
@@ -1,5 +1,8 @@
 import { names } from "./_users";
 
 export function GET(request: Request) {
-  return JSON.stringify(names);
+  return new Response(`Hello from ${process.env.VERCEL_REGION}`);
 }
+// export function GET(request: Request) {
+//   return JSON.stringify(names);
+// }
