7acb2dde391ddc15400b6b1db6a3f3eeb7ad8bb1
Wojciech Marek <marek.wojciech@outlook.com>
2024-06-23 13:27:53 +0200

    chore: update

--- api/hello.ts
+++ api/hello.ts
@@ -1,7 +1,9 @@
-import { names } from "./_users";
+const names: string[] = ["Steve"];
 
 export function GET(request: Request) {
-  return new Response(`Hello from ${process.env.VERCEL_REGION}`);
+  return new Response(
+    `Hello from ${process.env.VERCEL_REGION}, names: ${JSON.stringify(names)}`
+  );
 }
 // export function GET(request: Request) {
 //   return JSON.stringify(names);
