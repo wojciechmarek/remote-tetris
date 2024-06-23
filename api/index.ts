b011a32fc84608dd8ecd1abc18e664f547652ba9
Wojciech Marek <marek.wojciech@outlook.com>
2024-06-23 22:33:53 +0200

    chore: update 4

--- api/index.ts
+++ api/index.ts
@@ -45,7 +45,7 @@
 import express from "express"
 const app = express();
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+app.get("/hello", (req, res) => res.send("Express on Vercel"));
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
