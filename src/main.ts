cb3c65e079b1c5dd57a2323e13d872d74ea67106
Wojciech Marek <marek.wojciech@outlook.com>
2024-06-28 19:51:55 +0200

    feat: update vercel.json

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,9 @@ import "./app.css";
 import App from "./App.svelte";
 import { inject } from "@vercel/analytics";
 
-inject();
+if (import.meta.env.VERCEL_ENV === "production") {
+  inject();
+}
 
 const app = new App({
   target: document.getElementById("app")!,
