74ae030f003c7961762db75b390def8424fb6504
Wojciech Marek <marek.wojciech@outlook.com>
2024-07-13 19:44:51 +0200

    feat: add more STUN Servers

--- src/lib/utils/web-rtc.utils.ts
+++ src/lib/utils/web-rtc.utils.ts
@@ -2,9 +2,16 @@ export const WebRTCUtils = () => {
   //#region Peer Object Configuration
   const peer = new RTCPeerConnection({
     iceServers: [
-      {
-        urls: "stun:stun.l.google.com:19302"
-      }
+      { urls: "stun:stun.l.google.com:19302" },
+      { urls: "stun:stun.l.google.com:5349" },
+      { urls: "stun:stun1.l.google.com:3478" },
+      { urls: "stun:stun1.l.google.com:5349" },
+      { urls: "stun:stun2.l.google.com:19302" },
+      { urls: "stun:stun2.l.google.com:5349" },
+      { urls: "stun:stun3.l.google.com:3478" },
+      { urls: "stun:stun3.l.google.com:5349" },
+      { urls: "stun:stun4.l.google.com:19302" },
+      { urls: "stun:stun4.l.google.com:5349" }
     ]
   });
   //#endregion
